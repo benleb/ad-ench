717ad1ef7b1a1e28392ba61eb543d3077cbe91d8
Ben Lebherz <git@benleb.de>
2019-09-27 14:03:36 +0200

    initial release

---
+++ README.md
@@ -0,0 +1,39 @@
+# EnCh - Entity Checker
+
+[![hacs_badge](https://img.shields.io/badge/HACS-Custom-orange.svg)](https://github.com/custom-components/hacs)
+
+**NEEDS THE APPDAEMON MASTER/DEV BRANCH! Current stable (v3.0.5) will not work!**
+
+**REPLACES github.com/benleb/ad-batterycheck**
+
+*Entity check [AppDaemon](https://github.com/home-assistant/appdaemon) app which reads out the `battery_level` attribute and/or state (`unknown` or `unavailable` currently) of Home Assistant entities and sends a notification if desired.*
+
+## Installation
+
+Use [HACS](https://github.com/custom-components/hacs) or [download](https://github.com/benleb/ad-ench/releases) the `ench` directory from inside the `apps` directory here to your local `apps` directory, then add the configuration to enable the `ench` module.
+
+## App configuration
+
+```yaml
+ench:
+  module: ench
+  class: EnCh
+  notify: "notify.mobile_app"
+  show_friendly_name: False
+  battery:
+    interval: 15
+    min_level: 20
+  unavailable:
+    interval: 13
+```
+
+key | optional | type | default | description
+-- | -- | -- | -- | --
+`module` | False | string | ench | The module name of the app.
+`class` | False | string | EnCh | The name of the python class.
+`notify` | True | string | | The Home Assistant service used for notification
+`battery` | True | | | Set to enable low battery check
+`min_level` | True | integer | 20 | Minimum battery level a entity should have
+`interval` | True | integer | 18 | Hours between checks
+`unavailable` | True | | | Set to enable unavailable state check
+`interval` | True | integer | 18 | Hours between checks
---
+++ apps/ench/ench.py
@@ -0,0 +1,123 @@
+"""AppDaemon EnCh app.
+
+  @benleb / https://github.com/benleb/ad-ench
+
+ench:
+  module: ench
+  class: EnCh
+  battery_interval: 18
+  battery_min_level: 20
+  unavailable_interval: 6
+  notify: notify.me
+"""
+
+from datetime import timedelta
+from typing import Any, Dict
+
+import adutils
+import appdaemon.plugins.hass.hassapi as hass
+
+APP_NAME = "EnCh"
+APP_ICON = "👩‍⚕️"
+APP_VERSION = "0.1.0"
+
+DEFAULT_BATTERY_MIN_LEVEL = 20
+DEFAULT_INTERVAL = 18
+DEFAULT_INTERVAL_BATTERY = 18
+DEFAULT_INTERVAL_UNAVAILABLE = 6
+
+EXCLUDED_ENTITIES = ["persistent_notification.config_entry_discovery"]
+
+ICONS = dict(
+    battery="🔋",
+    unavailable="⁉️ ",
+    unknown="❓",
+)
+
+
+class EnCh(hass.Hass):  # type: ignore
+    """ench."""
+
+    def initialize(self) -> None:
+        """Register API endpoint."""
+        self.cfg: Dict[str, Any] = dict()
+        self.cfg["notify"] = self.args.get("notify")
+        self.cfg["show_friendly_name"] = bool(self.args.get("show_friendly_name", True))
+
+        # battery check
+        if "battery" in self.args:
+            self.cfg["battery"] = dict(
+                interval=self.args.get("battery").get("interval", DEFAULT_INTERVAL_BATTERY),
+                min_level=self.args.get("battery").get("min_level", DEFAULT_BATTERY_MIN_LEVEL),
+            )
+
+            # schedule check
+            self.run_every(
+                self.check_battery,
+                self.datetime() + timedelta(seconds=5),
+                self.cfg["battery"]["interval"] * 60 * 60,
+            )
+
+        # unavailable check
+        if self.args.get("unavailable"):
+            self.cfg["unavailable"] = dict(
+                interval=self.args.get("unavailable").get("interval", DEFAULT_INTERVAL_UNAVAILABLE),
+            )
+
+            self.run_every(
+                self.check_unavailable,
+                self.datetime() + timedelta(seconds=3),
+                self.cfg["unavailable"]["interval"] * 60 * 60,
+            )
+
+        adutils.show_info(self.log, APP_NAME, self.cfg, list(), icon=APP_ICON, appdaemon_version=self.get_ad_version())
+
+    def check_battery(self, _: Any) -> None:
+        """Handle scheduled checks."""
+        entities_low_battery: Dict[str, int] = dict()
+
+        for entity in self.get_state():
+            if entity.lower() in EXCLUDED_ENTITIES:
+                continue
+
+            try:
+                battery_level = self.get_state(entity_id=entity, attribute="battery_level")
+                # self.log(f"{entity} -> {battery_level}")
+                if battery_level and battery_level <= int(self.cfg["battery"]["min_level"]):
+                    self.log(f"{ICONS['battery']} Battery low on {entity} - {int(battery_level)}%", level="WARNING", ascii_encode=False)
+                    entities_low_battery[entity] = battery_level
+            except TypeError as error:
+                self.log(f"Getting state/battery level failed for {entity}: {error}")
+
+        # send notification
+        if self.cfg["notify"] and entities_low_battery:
+            self.call_service(
+                str(self.cfg["notify"]).replace(".", "/"),
+                message=f"{ICONS['battery']} Battery low ({len(entities_low_battery)}): {', '.join([e for e in entities_low_battery])}",
+            )
+
+    def check_unavailable(self, _: Any) -> None:
+        """Handle scheduled checks."""
+        entities_unavailable: Dict[str, str] = dict()
+
+        for entity in self.get_state():
+            if entity.lower() in EXCLUDED_ENTITIES:
+                continue
+
+            try:
+                state = self.get_state(entity_id=entity)
+                if state in ["unavailable", "unknown"] and entity not in entities_unavailable:
+                    entities_unavailable[entity] = state
+            except TypeError as error:
+                self.log(f"Getting state/battery level failed for {entity}: {error}")
+
+        for entity in sorted(entities_unavailable):
+            state = entities_unavailable[entity]
+            self.log(f"{ICONS[state]} State of \033[1m{entity}\033[0m{f' ({self.friendly_name(entity)})' if self.cfg['show_friendly_name'] else ''} is \033[1m{entities_unavailable[entity]}\033[0m!", level="WARNING", ascii_encode=False)
+
+        # send notification
+        if self.cfg["notify"] and entities_unavailable:
+            self.call_service(
+                str(self.cfg["notify"]).replace(".", "/"),
+                message=f"{APP_ICON} Unavailable entities ({len(entities_unavailable)}): {', '.join([e for e in entities_unavailable])}",
+            )
