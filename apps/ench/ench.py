6517c9e55517463a694b954db306daedc890d386
Ben Lebherz <git@benleb.de>
2020-01-14 13:58:59 +0100

    Bump version: 0.5.0 → 0.5.1

--- apps/ench/ench.py
+++ apps/ench/ench.py
@@ -31,7 +31,7 @@ from adutils import ADutils, hl, hl_entity
 
 APP_NAME = "EnCh"
 APP_ICON = "👩‍⚕️"
-APP_VERSION = "0.5.0"
+APP_VERSION = "0.5.1"
 
 BATTERY_MIN_LEVEL = 20
 INTERVAL_BATTERY_MIN = 180
